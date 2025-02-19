c85998a22504dabaaba677ebfbabb4020ffaa6a7
Alex Wilson <alex.dave999@gmail.com>
2025-02-19 21:28:03 +0000

    🐛 An attempt at a scroll behavior fix but it doesnt work

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,31 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter, RouteLocationNormalizedGeneric } from 'vue-router'
 
 import LandingPage from './pages/LandingPage.vue'
 import CorePrincaplesPage from './pages/CorePrincaplesPage.vue'
 
 export const routes = [
     { path: '/', component: LandingPage },
-    { path: '/core-princaples', component: CorePrincaplesPage },
+    { path: '/core-princaples', component: CorePrincaplesPage,
+      meta: { scrollToTop: true } },
   ];
   
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
+    scrollBehavior(to: RouteLocationNormalizedGeneric) {
+      if (to.hash) {
+        return {
+          el: to.hash,
+          behavior: 'smooth',
+        }
+      } 
+      
+      // Always scroll to top for routes with meta.scrollToTop
+      if (to.meta.scrollToTop) {
+        return {
+          top: 0,
+          behavior: 'smooth'
+        }
+      }
+    }
   })
